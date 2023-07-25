1dafcfa8cfcd1a950a4a5c1e85e34b54d718da62
taegung <sungminyun12@daum.net>
2023-07-25 14:12:41 +0900

    add func print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+import sys
+
 def print_message():
   message = "world"
   print("hello{}").format(message))
 
+def print_git():
+  message ="form the hell"
+  sys.print("git{}.format(message))
+
 if_name=="_main_":
    print_message()
+   print_git()
